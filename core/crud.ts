4494eff916c7fd24b9a28f5a80936ef6845bb00c
Beto <albertojuniorhc@gmail.com>
2023-05-12 15:26:19 -0700

    feat: enabled ts

---
+++ core/crud.ts
@@ -0,0 +1,10 @@
+import fs from "fs";
+
+const DB_FILE_PATH = "./core/db";
+
+function create(content: string) {
+  fs.writeFileSync(DB_FILE_PATH, content);
+  return content;
+}
+
+console.log(create("Starting CRUD!"));
