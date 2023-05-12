a5065dfad95b1dedee19fc2ded8c716e3bb9d2b9
Beto <albertojuniorhc@gmail.com>
2023-05-12 15:57:00 -0700

    feat: implementing read

--- core/crud.ts
+++ core/crud.ts
@@ -1,10 +1,42 @@
 import fs from "fs";
+console.clear();
 
 const DB_FILE_PATH = "./core/db";
 
+interface ToDo {
+  date: string;
+  content: string;
+  done: boolean;
+}
+
+function CLEAR_DB() {
+  fs.writeFileSync(DB_FILE_PATH, "");
+}
+
 function create(content: string) {
-  fs.writeFileSync(DB_FILE_PATH, content);
+  const toDo: ToDo = {
+    date: new Date().toISOString(),
+    content,
+    done: false,
+  };
+
+  const toDoList: Array<ToDo> = [...read(), toDo];
+
+  fs.writeFileSync(DB_FILE_PATH, JSON.stringify({ toDoList }, null, 2));
   return content;
 }
 
-console.log(create("Starting CRUD!"));
+function read(): Array<ToDo> {
+  const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
+  const db = JSON.parse(dbString || "{}");
+
+  if (!db.toDoList) return [];
+
+  return db.toDoList;
+}
+
+//Simulation
+CLEAR_DB();
+create("1st ToDo - CRUD");
+create("2nd ToDo - CRUD");
+console.log(read());
